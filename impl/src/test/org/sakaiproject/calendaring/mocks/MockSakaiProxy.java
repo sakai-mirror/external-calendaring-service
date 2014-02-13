498e3110518bb012e6068208ea3319495ac98df5
bkirschn@umich.edu <bkirschn@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2014-02-13 20:03:59 +0000

    INFRSTR-255 migrated from https://source.sakaiproject.org/contrib/external-calendaring-service (see contrib repo for svn log history)
    git-svn-id: https://source.sakaiproject.org/svn/external-calendaring-service/tags/external-calendaring-service-1.0.2@284956 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ impl/src/test/org/sakaiproject/calendaring/mocks/MockSakaiProxy.java
@@ -0,0 +1,44 @@
+package org.sakaiproject.calendaring.mocks;
+
+import org.sakaiproject.calendaring.logic.SakaiProxy;
+
+/**
+ * Mock of SakaiProxy so we can call the main service API
+ * 
+ * @author Steve Swinsburg (steve.swinsburg@gmail.com)
+ *
+ */
+public class MockSakaiProxy implements SakaiProxy {
+
+	
+	@Override
+	public String getCurrentUserId() {
+		return "abc123";
+	}
+
+	@Override
+	public boolean isSuperUser() {
+		return false;
+	}
+
+	@Override
+	public String getServerName() {
+		return "server_xyz";
+	}
+
+	@Override
+	public String getCalendarFilePath() {
+		return "/tmp";
+	}
+
+	@Override
+	public String getUserEmail(String uuid) {
+		return uuid + "@email.com";
+	}
+
+	@Override
+	public String getUserDisplayName(String uuid) {
+		return "User " + uuid;
+	}
+
+}
---
+++ impl/src/test/org/sakaiproject/calendaring/mocks/MockTimeService.java
@@ -0,0 +1,132 @@
+package org.sakaiproject.calendaring.mocks;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import org.sakaiproject.time.api.Time;
+import org.sakaiproject.time.api.TimeBreakdown;
+import org.sakaiproject.time.api.TimeRange;
+import org.sakaiproject.time.api.TimeService;
+
+public class MockTimeService implements TimeService {
+
+	@Override
+	public Time newTime(long millis) {
+		return new org.sakaiproject.mock.domain.Time(new Date(millis));
+	}
+	
+	@Override
+	public TimeRange newTimeRange(Time start, Time end, boolean startIncluded, boolean endIncluded) {
+		return new MockTimeRange(start, end, startIncluded, endIncluded);
+	}
+	
+	
+	/* anything below here is not used at this stage */
+
+	
+	@Override
+	public boolean clearLocalTimeZone(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean different(Time arg0, Time arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public GregorianCalendar getCalendar(TimeZone arg0, int arg1, int arg2,
+			int arg3, int arg4, int arg5, int arg6, int arg7) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeZone getLocalTimeZone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time newTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	@Override
+	public Time newTime(GregorianCalendar arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeBreakdown newTimeBreakdown(int arg0, int arg1, int arg2,
+			int arg3, int arg4, int arg5, int arg6) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time newTimeGmt(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time newTimeGmt(TimeBreakdown arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time newTimeGmt(int arg0, int arg1, int arg2, int arg3, int arg4,
+			int arg5, int arg6) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time newTimeLocal(TimeBreakdown arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time newTimeLocal(int arg0, int arg1, int arg2, int arg3, int arg4,
+			int arg5, int arg6) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeRange newTimeRange(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeRange newTimeRange(Time arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeRange newTimeRange(long arg0, long arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeRange newTimeRange(Time arg0, Time arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
